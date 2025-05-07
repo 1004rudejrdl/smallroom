cd605daf81717b23c29886730a439207fc4fde12
DESKTOP-GM0DTCG\82108 <1004rudejrdl@naver.com>
2025-05-07 12:46:56 +0900

    commit2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Image from './smallroom1.png';
 import Image2 from './smallroom2.png';
-const logo = <img src={Image} alt="로고"></img>;
-const logo2 = <img src={Image2} alt="로고"></img>;
+const logo = <img src={Image} alt="로고" className="test1"></img>;
+const logo2 = <img src={Image2} alt="로고" className="test1"></img>;
 
 function App() {
   return (
       <div className="App">
 
-        <div>{logo}</div>
-        <div>{logo2}</div>
+        <div >{logo}</div>
+        <div >{logo2}</div>
 
 
 
